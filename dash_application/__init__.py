736215c6374ae38c746083ef4056d097f25ab9a4
Kilian Martin <kilian.martin@me.com>
2021-03-23 11:10:19 +0100

    Final version

--- dash_application/__init__.py
+++ dash_application/__init__.py
@@ -9,6 +9,7 @@ import requests
 import json
 
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 
 def k1_fetch():
@@ -40,7 +41,8 @@ def k1_fetch():
     return k1_df
 
 def k2_fetch():
-    k2_endpoint = "https://qovo4nsf3oonbax-db202103111252.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip_rose/kpi2/incsolved/" ##REPLACE WITH MINE!
+    #k2_endpoint = "https://qovo4nsf3oonbax-db202103111252.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip_rose/kpi2/incsolved/" ##REPLACE WITH MINE!
+    k2_endpoint = "https://eydvdcrr2v9awbf-iboardadw.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip/kpi2/incsolved/" ##REPLACE WITH MINE!
 
     k2_r = requests.get(k2_endpoint)
     k2_kpi_data = k2_r.json()["items"]
@@ -68,13 +70,20 @@ def k2_fetch():
     return k2_df
 
 def k3_fetch(month_selected):
+    """
     if month_selected == "Mar": 
         k3_endpoint = "https://qovo4nsf3oonbax-db202103111252.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip_rose/kpi3/sla/201803" ##REPLACE LATER
     elif month_selected == "Feb":
         k3_endpoint = "https://qovo4nsf3oonbax-db202103111252.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip_rose/kpi3/sla/201802" ##REPLACE LATER
     else:
         k3_endpoint = "https://qovo4nsf3oonbax-db202103111252.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip_rose/kpi3/sla/201801" ##REPLACE LATER
-
+    """
+    if month_selected == "Mar": 
+        k3_endpoint = "https://eydvdcrr2v9awbf-iboardadw.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip/kpi3/sla/201803" ##REPLACE LATER
+    elif month_selected == "Feb":
+        k3_endpoint = "https://eydvdcrr2v9awbf-iboardadw.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip/kpi3/sla/201802" ##REPLACE LATER
+    else:
+        k3_endpoint = "https://eydvdcrr2v9awbf-iboardadw.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip/kpi3/sla/201801" ##REPLACE LATER
 
 
     k3_r = requests.get(k3_endpoint)
@@ -107,7 +116,8 @@ def k3_fetch(month_selected):
 
 def k4_fetch():
     
-    k4_endpoint = "https://qovo4nsf3oonbax-db202103111252.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip_rose/kpi4/BL/"
+    #k4_endpoint = "https://qovo4nsf3oonbax-db202103111252.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip_rose/kpi4/BL/"
+    k4_endpoint = "https://eydvdcrr2v9awbf-iboardadw.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip/kpi4/BL/"
 
     k4_r = requests.get(k4_endpoint)
     k4_kpi_data = k4_r.json()["items"]
@@ -140,8 +150,12 @@ def k5_fetch():
     k5_endpoint_1 = "https://qovo4nsf3oonbax-db202103111252.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip_rose/kpi5/av/201801"
     k5_endpoint_2 = "https://qovo4nsf3oonbax-db202103111252.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip_rose/kpi5/av/201802"
     k5_endpoint_3 = "https://qovo4nsf3oonbax-db202103111252.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip_rose/kpi5/av/201803"
+    """
 
-    endpoints = [k5_endpoint_1, k5_endpoint_2, k5_endpoint_3]
+    k5_endpoint_1 = "https://eydvdcrr2v9awbf-iboardadw.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip/kpi5/av/201801"
+    k5_endpoint_2 = "https://eydvdcrr2v9awbf-iboardadw.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip/kpi5/av/201802"
+    k5_endpoint_3 = "https://eydvdcrr2v9awbf-iboardadw.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip/kpi5/av/201803"
+    endpoints = [k5_endpoint_3, k5_endpoint_2, k5_endpoint_1]
 
 
     months = []
@@ -165,17 +179,22 @@ def k5_fetch():
             av_per = dict["availability_percentage"]
 
             output_dict[month][service] = [unav_time, av_per]
-    """
     
-    output_dict = {'201801': {'.COM': [2.3, 0.9968493150684932], 'FLIGHT DISPATCHING': [1.6166666666666667, 0.9977853881278539], 'FLIGHT TRACKING AND CONTROL': [4, 0.9945205479452055], 'MAD-HUB OPERATIONAL MANAGEMENT SYSTEM': [8.55, 0.9882876712328768]}, '201802': {'.COM': [2.8666666666666667, 0.9960730593607305], 'MAD-HUB OPERATIONAL MANAGEMENT SYSTEM': [8.683333333333334, 0.9881050228310503], 'STATIONS OPERATIONAL MANAGEMENT SYSTEM': [11.716666666666667, 0.9839497716894977]}, '201803': {'.COM': [3.933333333333333, 0.9946118721461187], 'FLIGHT DISPATCHING': [2.966666666666667, 0.9959360730593607], 'STATIONS OPERATIONAL MANAGEMENT SYSTEM': [2.533333333333333, 0.9965296803652968]}}
+    
+    #output_dict = {'201801': {'.COM': [2.3, 0.9968493150684932], 'FLIGHT DISPATCHING': [1.6166666666666667, 0.9977853881278539], 'FLIGHT TRACKING AND CONTROL': [4, 0.9945205479452055], 'MAD-HUB OPERATIONAL MANAGEMENT SYSTEM': [8.55, 0.9882876712328768]}, '201802': {'.COM': [2.8666666666666667, 0.9960730593607305], 'MAD-HUB OPERATIONAL MANAGEMENT SYSTEM': [8.683333333333334, 0.9881050228310503], 'STATIONS OPERATIONAL MANAGEMENT SYSTEM': [11.716666666666667, 0.9839497716894977]}, '201803': {'.COM': [3.933333333333333, 0.9946118721461187], 'FLIGHT DISPATCHING': [2.966666666666667, 0.9959360730593607], 'STATIONS OPERATIONAL MANAGEMENT SYSTEM': [2.533333333333333, 0.9965296803652968]}}
 
     return output_dict
 
 def k6_fetch():
-    
+    """
     k6_endpoint_1 = "https://qovo4nsf3oonbax-db202103111252.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip_rose/kpi6/monav/201801"
     k6_endpoint_2 = "https://qovo4nsf3oonbax-db202103111252.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip_rose/kpi6/monav/201802"
-    k6_endpoint_3 = "https://qovo4nsf3oonbax-db202103111252.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip_rose/kpi6/monav/201803"
+    k6_endpoint_3 = "https://qovo4nsf3oonbax-db202103111252.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip_rose/kpi6/monav/201803" 
+    """
+    
+    k6_endpoint_1 = "https://eydvdcrr2v9awbf-iboardadw.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip/kpi6/monav/201801"
+    k6_endpoint_2 = "https://eydvdcrr2v9awbf-iboardadw.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip/kpi6/monav/201802"
+    k6_endpoint_3 = "https://eydvdcrr2v9awbf-iboardadw.adb.eu-frankfurt-1.oraclecloudapps.com/ords/tip/kpi6/monav/201803"
 
     endpoints = [k6_endpoint_1, k6_endpoint_2, k6_endpoint_3]
 
@@ -209,29 +228,31 @@ def k6_fetch():
 def create_dash_kpi1(flask_app):
 
     
-    dash_app = dash.Dash(server=flask_app, name="Dashboard", url_base_pathname=("/kpi1/"))
+    dash_app = dash.Dash(server=flask_app, name="Dashboard", url_base_pathname=("/kpi1/"), external_stylesheets=external_stylesheets)
 
 
     dash_app.layout = html.Div(children=[
-        html.H1(children='KPI1'),
+        html.H1(children='Number of incidents - by priority'),
 
         html.Div(children='''
-            Showing # of incidents
+            How many incidents do we have and how severe are they?
         '''),
 
         dcc.Graph(
             id='kpi1',
-            figure= px.bar(k1_fetch(), x="Months", y="Number of incidents", color="Priority", barmode="group")
+            figure= px.bar(k1_fetch(), x="Months", y="Number of incidents", color="Priority", barmode="group"),
+            
 
 
         )
-    ])
+    ], style={'font-family': ' Arial, Helvetica, sans-serif'})
     
-
+    
     for view_function in dash_app.server.view_functions:
         if view_function.startswith(dash_app.config.url_base_pathname):
             dash_app.server.view_functions[view_function] = login_required(
                 dash_app.server.view_functions[view_function])
+    
     
     return dash_app
 
@@ -252,9 +273,9 @@ def create_dash_kpi2(flask_app):
             figure= px.bar(k2_fetch(), x="Months", y="Number of incidents", barmode="group")
 
         )
-    ])
+    ], style={'font-family': ' Arial, Helvetica, sans-serif'})
     
-
+    
     for view_function in dash_app.server.view_functions:
         if view_function.startswith(dash_app.config.url_base_pathname):
             dash_app.server.view_functions[view_function] = login_required(
@@ -270,23 +291,23 @@ def create_dash_kpi3(flask_app):
 
 
     dash_app.layout = html.Div(children=[
-        html.H1(children='KPI3 - Jan 2018'),
+        html.H1(children='SLA Adherence - March 2018'),
 
         html.Div(children='''
-            XXX
+            Are we adhering to our SLAs?
         '''),
 
         dcc.Graph(
-            id='kpi3-1',
+            id='kpi3-3',
             figure= px.bar(k3_fetch("Jan"), x="Priority", y="Percent", color="SLA adherence", barmode="group")
 
 
         ),
 
-        html.H1(children='KPI3 - Feb 2018'),
+        html.H1(children='SLA Adherence - Feb. 2018'),
 
         html.Div(children='''
-            XXX
+            Are we adhering to our SLAs?
         '''),
 
         dcc.Graph(
@@ -296,22 +317,22 @@ def create_dash_kpi3(flask_app):
 
         ),
 
-        html.H1(children='KPI3 - Mar 2018'),
+        html.H1(children='SLA Adherence - Jan. 2018'),
 
         html.Div(children='''
-            XXX
+            Are we adhering to our SLAs?
         '''),
 
         dcc.Graph(
-            id='kpi3-3',
+            id='kpi3-1',
             figure= px.bar(k3_fetch("Mar"), x="Priority", y="Percent", color="SLA adherence", barmode="group")
 
 
         )
 
-    ])
+    ], style={'font-family': ' Arial, Helvetica, sans-serif'})
     
-
+    
     for view_function in dash_app.server.view_functions:
         if view_function.startswith(dash_app.config.url_base_pathname):
             dash_app.server.view_functions[view_function] = login_required(
@@ -325,10 +346,10 @@ def create_dash_kpi4(flask_app):
 
 
     dash_app.layout = html.Div(children=[
-        html.H1(children='KPI4'),
+        html.H1(children='Backlog of incidents'),
 
         html.Div(children='''
-            XXX
+            How many incidents do we have in our backlog?
         '''),
 
         dcc.Graph(
@@ -336,9 +357,9 @@ def create_dash_kpi4(flask_app):
             figure= px.bar(k4_fetch(), x="Months", y="Number of incidents", barmode="group")
 
         )
-    ])
+    ], style={'font-family': ' Arial, Helvetica, sans-serif'})
     
-
+    
     for view_function in dash_app.server.view_functions:
         if view_function.startswith(dash_app.config.url_base_pathname):
             dash_app.server.view_functions[view_function] = login_required(
@@ -353,14 +374,21 @@ def create_dash_kpi5_constructor(k5_dict):
     children = []
     
     for month in k5_dict.keys():
-        children.append(html.H1(children=month))
+        
+        months_year = month[0:4]
+        months_month = month[4:]
+        
+        
+        children.append(html.H1(children=f'Critical services in {months_month}/{months_year}'))
         
         for service in k5_dict[month].keys():
-            children.append(html.H2(children=service))
+            
+            service_array = []
+            service_array.append(html.H2(children=f'{months_month}/{months_year}: {service}'))
             
             kpis = k5_dict[month][service]
 
-            children.append(html.P(children=f'Downtime: {kpis[0]} mins'))
+            service_array.append(html.P(children=f'Downtime: {round(kpis[0], 2)} mins'))
             
             kpi_list1 = [kpis[1] * 100]
 
@@ -369,13 +397,24 @@ def create_dash_kpi5_constructor(k5_dict):
             })
             
 
-            children.append(dcc.Graph(
+            service_array.append(dcc.Graph(
                 figure= px.bar(kpi_df, x="% Availability", 
                 orientation='h', height=200, range_x=[97.5, 100])
-                .update_yaxes(showticklabels=False, title_font_color="#FFFFFF")))
+                .update_yaxes(showticklabels=False, title_font_color="rgba(0, 0, 0, 0)")
+                .update_layout({'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+                })
 
+
+                ))
+
+            service_div = html.Div(service_array, style={'background-color': '#f7f7f7', 'padding': '10px', 'margin-bottom': '10px'})
+            
+            children.append(service_div)
 
     return children
+
+
+
 
 
 def create_dash_kpi5(flask_app):
@@ -385,10 +424,10 @@ def create_dash_kpi5(flask_app):
 
     children = create_dash_kpi5_constructor(k5_dict)
 
-    dash_app.layout = html.Div(children)
+    dash_app.layout = html.Div(children, style={'font-family': ' Arial, Helvetica, sans-serif'})
     
     
-
+    
     for view_function in dash_app.server.view_functions:
         if view_function.startswith(dash_app.config.url_base_pathname):
             dash_app.server.view_functions[view_function] = login_required(
@@ -402,10 +441,10 @@ def create_dash_kpi6(flask_app):
 
 
     dash_app.layout = html.Div(children=[
-        html.H1(children='KPI6'),
+        html.H1(children='Monthly Availability of critical services'),
 
         html.Div(children='''
-            XXX
+            How well are our most critical services available?
         '''),
 
         dcc.Graph(
@@ -413,9 +452,9 @@ def create_dash_kpi6(flask_app):
             figure= px.bar(k6_fetch(), x="Months", y="Monthly Availability (in %)", barmode="group", range_y=[90, 100])
 
         )
-    ])
+    ], style={'font-family': ' Arial, Helvetica, sans-serif'})
     
-
+    
     for view_function in dash_app.server.view_functions:
         if view_function.startswith(dash_app.config.url_base_pathname):
             dash_app.server.view_functions[view_function] = login_required(
--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask_migrate import upgrade, Migrate
 from flask_login import LoginManager, login_user, logout_user, login_required
 from flask_login.mixins import UserMixin
+
+import pymysql
 
 from wtforms import StringField, PasswordField
 from wtforms.validators import Length, Email
@@ -15,7 +17,14 @@ from dash_application import create_dash_kpi1, create_dash_kpi2, create_dash_kpi
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "THIS IS A SECRET"       ##REPLACE LATER
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'   ##REPLACE WITH GCLOUD SQL INSTANCE
+
+#LOCAL SETUP
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
+
+#CLOUD SETUP
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:Ic8JErjp39oaGwEe@/users?unix_socket=/cloudsql/tip-kilian-martin:europe-west1:tip-kilian-martin"
+
+
 
 Bootstrap(app)
 db = SQLAlchemy(app)
@@ -46,20 +55,22 @@ class User(db.Model, UserMixin):
 
 
 class LoginForm(FlaskForm):
-    email = StringField("email", validators=[Email()])
-    password = PasswordField("password", validators=[Length(min=5)])
+    email = StringField("Email", validators=[Email()])
+    password = PasswordField("Password", validators=[Length(min=5)])
 
 
 class RegisterForm(FlaskForm):
-    email = StringField("email", validators=[Email()])
-    password = PasswordField("password", validators=[Length(min=5)])
-    repeat_password = PasswordField("repeated_password", validators=[Length(min=5)])
+    email = StringField("Email", validators=[Email()])
+    password = PasswordField("Password", validators=[Length(min=5)])
+    repeat_password = PasswordField("Repeate Password", validators=[Length(min=5)])
 
 
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/index/<item_type>")
+def index(item_type="All"):
+    
+    return render_template("index.html", item_type=item_type)
 
 @app.route("/login", methods = ["GET", "POST"])
 def login():
@@ -103,10 +114,29 @@ def logout():
 
 @app.route("/dashboards/<dashboard>")
 def dashboards(dashboard):
-    print(dashboard, flush=True)
+    #print(dashboard, flush=True)
 
     return render_template("dashboards.html", dashboard=dashboard)
 
 
+@app.route('/admin/dbupgrade/<key>')      ##Consider replacing; not very secure
+def dbupgrade():
+    if key == "jsisid83jd8dajkdsjkdsjk":
+        migrate = Migrate(app, db)
+        upgrade(directory=migrate.directory)
+        return 'migrated'
+    else:
+        return 'unauthorized'
+
+
+@app.route("/slides")
+def slides():
+
+    return render_template("slides.html")
+
+
+"""
+
 if __name__ == "__main__":
     app.run(debug=True)
+"""
